ba14ff3ee1481e1a3a3ffc750ae6de26549aa239
Duaa74 <duaasayed74@gmail.com>
2023-10-16 13:47:32 -0700

    a

---
+++ test/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("Hello, World!\n");
+	char c = 'A';
+	_printf("Character: %c\n", c);
+	_printf("This is a string: %s\n", "I am a string.");
+
+	return (0);
+}
